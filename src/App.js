12cd291f1a053b3b87b28aea0c473e297b4d984f
Sayantani96 <sayantani.h96@gmail.com>
2022-07-22 18:22:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, {useEffect} from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {FiSettings} from 'react-icons/fi'
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+
+import './App.css'
+
+const App = () => {
+  const activeMenu=true;
+  return (
+    <div>
+       <BrowserRouter>
+       <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed bottom-4 right-4" style={{zIndex:'1000'}}>
+            <TooltipComponent content="Settings" position="Top">
+                <button type="button" className="text-3xl p-4 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{backgroundColor:'blue',borderRadius:'50%'}}>
+                    <FiSettings/>
+                </button>
+            </TooltipComponent>
+        </div>
+        {
+          activeMenu? 
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            Sidebar w-0
+          </div> :
+          <div className="w-0 dark:bg-secondary-dark-bg bg-white">
+            Sidebar w-0
+          </div>
+        }
+        <div className={activeMenu? 'dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full':
+     'dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2' }>
+          <div className='fixed md-static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                  Navbar
+          </div>
+        </div>
+        <Routes>
+          {/*Dashboard*/}
+            <Route path="/" element="ECommerce"/>
+            <Route path="/ecommerce" element="ECommerce"/>
+
+            {/*Pages*/}
+            <Route path="/orders" element="Orders"/>
+            <Route path="/employees" element="Employees"/>
+            <Route path="/customers" element="Customers"/>
+
+            {/*Apps*/}
+            <Route path="/kanban" element="Kanban"/>
+            <Route path="/editor" element="Editors"/>
+            <Route path="/calendar" element="Calendar"/>
+            <Route path="/color-picker" element="ColorPicker"/>
+
+             {/*Charts*/}
+            <Route path="/line" element="Line"/>
+            <Route path="/area" element="Area"/>
+            <Route path="/bar" element="Bar"/>
+            <Route path="/pie" element="Pie"/>
+            <Route path="/financial" element="Finance"/>
+            <Route path="/color-mapping" element="ColorMapping"/>
+            <Route path="/pyramid" element="Pyramid"/>
+            <Route path="/stacked" element="Stacked"/>
+          </Routes>
+       </div>
+       </BrowserRouter>
+        </div>
+  )
+}
+
+export default App;
